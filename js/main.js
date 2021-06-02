dad457aec03464ee9bf3da591f1a5022f0a7a6f1
tiheryv <tiheryvr@gmail.com>
2021-06-02 13:21:49 -0500

    ADD Introduction JS

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
 console.log("Hola Koders");
+
+let nameKoder = prompt('Escribe tu nombre: ')
+console.log(nameKoder)
+
+let ageKoder = prompt('Digita tu edad: ')
+
+let resultSuma = parseInt(ageKoder) + 5
+console.log('suma: ', resultSuma)
+console.log('resta: ', ageKoder-5)
+console.log('div: ', ageKoder/5)
+console.log('mult: ', ageKoder*5)
+
