c22c2d27227b2c22ffcb35c501d4319de4ed7c96
tiheryv <tiheryvr@gmail.com>
2021-06-21 12:35:11 -0500

    ADD if and operators exercises

---
+++ js/triangleArea.js
@@ -0,0 +1,5 @@
+let base = prompt("Digite la base del triangulo")
+let high =  prompt("Digite la altura del triangulo")
+
+let areaTriangle = (base*high)/2
+alert(`El area del triangulo es ${areaTriangle}`)
