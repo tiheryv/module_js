e607663645e93e064cfda519f65444fd3045c3e6
tiheryv <tiheryvr@gmail.com>
2022-11-29 23:55:05 -0600

    Deleted mentors list

--- js/main.js
+++ js/main.js
@@ -1,32 +1,39 @@
-var heading = document.createElement("h1")
-document.body.appendChild(heading)
+//Seleccionar elemnetos a través de tagname
 
-console.log(heading)
+let listItems = document.getElementsByTagName("li")
+console.log(listItems)
 
-var headingText = document.createTextNode("Hola Koders")
-heading.appendChild(headingText)
+// Selecionar elementos a través de su clase
 
+let successItems = document.getElementsByClassName("bg-success")
+console.log(successItems)
 
-var namesArray = [
-    "Beto",
-    "Gustavo",
-    "Mariana"
+// Seleccionar elementos a través de su id
+
+let kodersList = document.getElementById("koders-list")
+console.log(kodersList)
+
+//Selector de últiples elementos
+
+document.querySelector()
+document.querySelectorAll()
+
+let notesArray = [
+    {
+        signature:"Español",
+        note: 8
+    },
+    {
+        signature:"Matemáticas",
+        note: 9
+    },
+    {
+        signature:"Inglés",
+        note: 8
+    },
+    {
+        signature:"Física",
+        note: 10
+    },
+
 ]
-
-
-var list = document.createElement("ul")
-document.body.appendChild(list)
-
-console.log(list)
-
-namesArray.forEach( item => {
-
-    var listElement = document.createElement("li")
-    var itemText = document.createTextNode(item)
-    
-    listElement.appendChild(itemText)
-    list.appendChild(listElement)
-
-})
-
-document.body.appendChild(list)
